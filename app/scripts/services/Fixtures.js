f1a22c85dd2323a7778361ba39350d5193415eef
Dan Kang <dtkang@gmail.com>
2017-04-02 23:23:05 +0000

    Assignment angular services pt1 complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -34,6 +34,16 @@
         Fixtures.getAlbum = function() {
             return albumPicasso;
         };
+        
+        Fixtures.getCollection = function(numberOfAlbums) {
+            var albumArray = [];
+            
+            for (var i=0; i<numberOfAlbums; i++) {
+                albumArray.push(albumPicasso);
+            }
+            
+            return albumArray;
+        };
 
         return Fixtures;
     }
